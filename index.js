2307b5d02cbd251c531489ac250b36e4d987d6b6
Imran <webimran2021@gmail.com>
2023-10-20 20:35:46 +0600

    brand details added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -35,6 +35,126 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+        // geting loreal product
+        app.get('/product/loreal', async (req, res) => {
+
+            try {
+                const products = await productCollection.find({ brand: "Loreal" }).toArray();
+
+                res.json(products);
+            } catch (error) {
+                console.error('Error finding products:', error);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        });
+
+        // geting loreal product
+        app.get('/product/loreal', async (req, res) => {
+
+            try {
+                const products = await productCollection.find({ brand: "Loreal" }).toArray();
+
+                res.json(products);
+            } catch (error) {
+                console.error('Error finding products:', error);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        });
+
+
+        // geting Estee Lauder product
+        app.get('/product/estee_lauder', async (req, res) => {
+
+            try {
+                const products = await productCollection.find({ brand: "Estee Lauder" }).toArray();
+
+                res.json(products);
+            } catch (error) {
+                console.error('Error finding products:', error);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        });
+
+        // geting Chanel Lauder product
+        app.get('/product/chanel', async (req, res) => {
+
+            try {
+                const products = await productCollection.find({ brand: "Chanel" }).toArray();
+
+                res.json(products);
+            } catch (error) {
+                console.error('Error finding products:', error);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        });
+
+        // geting Revlon Lauder product
+        app.get('/product/revlon', async (req, res) => {
+
+            try {
+                const products = await productCollection.find({ brand: "Revlon" }).toArray();
+
+                res.json(products);
+            } catch (error) {
+                console.error('Error finding products:', error);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        });
+
+        // geting Avon Lauder product
+        app.get('/product/avon', async (req, res) => {
+
+            try {
+                const products = await productCollection.find({ brand: "Avon" }).toArray();
+
+                res.json(products);
+            } catch (error) {
+                console.error('Error finding products:', error);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        });
+
+        // geting Dior Lauder product
+        app.get('/product/dior', async (req, res) => {
+
+            try {
+                const products = await productCollection.find({ brand: "Dior" }).toArray();
+
+                res.json(products);
+            } catch (error) {
+                console.error('Error finding products:', error);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        });
+
+        // geting indivisual product
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            console.log(query);
+            res.send(result);
+        });
+
+        // post methode
+        app.post("/cart", async (req, res) => {
+            const myCart = req.body;
+            console.log(myCart);
+            const result = await productCollection.insertOne(myCart);
+            res.send(result);
+        });
+
+        // cart method
+        app.get("/cart", async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+
+
+
 
         app.post('/product', async (req, res) => {
             const newProduct = req.body;
